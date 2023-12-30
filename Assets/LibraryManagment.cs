6b5cee7a24557bd57471655c82b5a6db491953ff
paoza33 <mehdi.kar@hotmail.fr>
2023-12-30 20:19:49 +0100

    test fusion 2

--- Assets/LibraryManagment.cs
+++ Assets/LibraryManagment.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class LibraryManagment : MonoBehaviour
 {
-<<<<<<< Updated upstream
     public int[] order = new int[5];
-=======
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
->>>>>>> Stashed changes
 }
